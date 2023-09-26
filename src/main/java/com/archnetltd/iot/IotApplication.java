e703f07d585f7c9105262785a547cd84f3130e0c
unknown <kureem@gmail.com>
2023-09-26 23:29:49 +0400

    impl

--- src/main/java/com/archnetltd/iot/IotApplication.java
+++ src/main/java/com/archnetltd/iot/IotApplication.java
@@ -1,97 +1,17 @@
 package com.archnetltd.iot;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.annotation.MessagingGateway;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.core.MessageProducer;
-import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
-import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
-import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
-import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
-import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
 
 @SpringBootApplication
 public class IotApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context =SpringApplication.run(IotApplication.class, args);
-		MyGateway gateway = context.getBean(MyGateway.class);
-        gateway.sendToMqtt("foo");
+		//MyGateway gateway = context.getBean(MyGateway.class);
+        //gateway.sendToMqtt("foo");
         
 	}
-	@Bean
-    public MessageChannel mqttInputChannel() {
-        return new DirectChannel();
-    }
-
-    @Bean
-    public MessageProducer inbound() {
-        MqttPahoMessageDrivenChannelAdapter adapter =
-                new MqttPahoMessageDrivenChannelAdapter("ssl://cbbb3116.ala.us-east-1.emqxsl.com:8883", "publish_client",
-                                                 "switching");
-        //adapter.set
-        adapter.setCompletionTimeout(5000);
-        adapter.setConverter(new DefaultPahoMessageConverter());
-        adapter.setQos(0);
-        adapter.setOutputChannel(mqttInputChannel());
-        return adapter;
-    }
-
-    @Bean
-    @ServiceActivator(inputChannel = "mqttInputChannel")
-    public MessageHandler handler() {
-        return new MessageHandler() {
-
-            @Override
-            public void handleMessage(Message<?> message) throws MessagingException {
-                System.out.println(message.getPayload());
-            }
-
-        };
-    }
-
-    @Bean
-    public MqttPahoClientFactory mqttClientFactory() {
-        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
-        MqttConnectOptions options = new MqttConnectOptions();
-        options.setServerURIs(new String[] { "ssl://cbbb3116.ala.us-east-1.emqxsl.com:8883" });
-        
-        options.setUserName("kureem");
-        options.setPassword("password".toCharArray());
-        factory.setConnectionOptions(options);
-        
-        return factory;
-    }
-    @Bean
-    @ServiceActivator(inputChannel = "mqttOutboundChannel")
-    public MessageHandler mqttOutbound() {
-        MqttPahoMessageHandler messageHandler =
-                       new MqttPahoMessageHandler("publish_client", mqttClientFactory());
-        messageHandler.setAsync(true);
-        
-        messageHandler.setDefaultTopic("switching");
-        return messageHandler;
-    }
-
-    @Bean
-    public MessageChannel mqttOutboundChannel() {
-        return new DirectChannel();
-    }
-
-    @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
-    public interface MyGateway {
-
-        void sendToMqtt(String data);
-
-    }
+	
 }
